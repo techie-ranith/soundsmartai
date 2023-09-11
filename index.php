62484ca291a499b89f38a047380b506bf26f2bba
Ranith <ranithgunawardana123@gmail.com>
2023-09-11 22:04:45 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include "navbar.php";
+    ?>
+    <div class="contain2">
+        <img src="assets/images/voice wave.gif" class="frontpic" alt="">
+        <selection class="toptxt">
+            <div class="maxwidth">
+                <div >
+                    <p >CONVERT <span class="Efficient">EFFICIENT</span><br>
+                        SPEACH TO TEXT<br></p>
+                </div>
+                <div class="contain3">
+                    <p class="para1">We’re building superhuman systems for transcribing and understanding human speech</p>
+                </div>
+                <div>
+                    <button class="tryit">Try it out</button>
+                </div>
+            </div>
+        </selection>
+    </div>
+    <!-- end of front pages -->
+    <!-- 2nd secctor -->
+    <div>
+        <div class="back1">
+            
+            <div class="hithere">
+                <p>Hi there, <br>
+                    Whats going on...</p>
+        
+            </div>
+            <div class="playbut">
+                <img src="assets/images/playbutton.png" alt="">
+        
+            </div>
+            <div class="voiceimg">
+                <!-- <img src="" alt="image"> -->
+            </div>
+    </div>
+    
+</body>
+</html>

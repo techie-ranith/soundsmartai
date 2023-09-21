2a89a49566f806e8cafc536648153b0641fa4e02
yohan <warushayoha80@gmail.com>
2023-09-21 20:51:07 +0530

    add logo to link from home page

--- html/navbar.php
+++ html/navbar.php
@@ -15,7 +15,7 @@
 <body>
    <!-- TOP BAR -->
    <div class="topbar">   
-    <a  class="home" href="../html/index.html" >Home</a>
+    <a  class="home" href="../html/index.php" >Home</a>
     <a  href="#support">Support</a>
     <a  href="../html/pricing.php">Pricing</a>
     <a  href="#about">About</a>
@@ -23,7 +23,7 @@
     <a  href="#signup">SignUp</a>
     
 </div> 
-<a href=""><div class="logos"><img src="../assest/01.png" ></div>      </a>         
+<a href="../html/index.php"><div class="logos"><img src="../assest/01.png" ></div>      </a>         
 
 
 

399b7fbe7e265c62f4b49c8a8d297f85340543aa
Yashira-De-Silva <yashiradesilva@gmail.com>
2023-09-23 16:40:33 +0530

    add this i code sign in page and css page

--- siginin.php
+++ siginin.php
@@ -4,41 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="./css/siginin.css">
+</head>
+
+
 <body>
+
 <?php
     include "navbar.php";
       ?>
+<div class="sigin">
+  <img src="./assest/signin.jpg" alt="">
+</div>
 
-<div class="wrapper">
-        <form name="SignInForm" method="post" action="your-action-url.php">
-            <h1>Sign In</h1>
-            E-Mail:<br>
-            <input type="text" name="txtmne"> 
-            <i class='bx bx-envelope'></i><br>
-            Password:<br>
-            <input type="password" name="pwd">
-            <i class='bx bxs-lock-alt'></i><br> 
-            <br>
-            <button type="submit" class="btn">SIGN IN</button>
-
-            <div class="register-link">
-               Don't have an Account? <a href="#">Sign Up</a>
-            </div>
-        </form>
-    </div>
+<div class="content">
+                     <div class="topic">
+                               <p>Sign In</p>
+                    </div>
+                
+                     <div class="form1" method="post" action="your-action-url.php">
 
 
+                            <label for="">Username</label><br>
+                           <input type="text" class="use" placeholder="Enter here ..." ><br>
+                           <br>
+                           <label for="">Password</label><br>
 
+                           <input type="password" class="pas" placeholder="Enter Password here ... " >
 
+                           <br>
 
 
 
 
 
 
-
+                    </div>
 
 
 
@@ -54,6 +58,22 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+<button type="submit" class="btn">SIGN IN</button>
 
 <?php include "footer.php"; ?>
 

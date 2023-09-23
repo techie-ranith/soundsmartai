fffd5ea3a62276525fcb7175e9bfa5329c778b9a
yohan manohara <warushayohan80@gmail.com>
2023-09-23 16:51:03 +0530

    meths funtion page add on

---
+++ function.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/function.css">
+    <!--css import-->
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/navbar.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <!-- navbar import -->
+    <div class="topbar">   
+        <div><img src="assets/images/logo.png "class="logos" ></div>
+        <a  href="#home">Home</a>
+        <a  href="#support">Support</a>
+        <a  href="#pricing">Pricing</a>
+        <a  href="#about">About</a>
+        <a  href="#signin">Signin</a>
+        <a  href="#signup">SignUp</a>
+        
+</div> 
+<div class="maincontain">
+    <div class="text1">DRAG HERE</div><br>
+    <div class="func">Drag & Drop<br>or Browse</div>
+    <div class="sub">Submit</div>
+        
+    </div>
+</div>
+
+    <!--footer import-->
+    <div class="footer">
+        <div class="footer-section">
+            <div class="box">Quick Links <br> <br> Pricing <br>SignUp</div>
+            <div class="box">Contact <br> soundsmartai@gmail.com</div>
+            <div class="box box3">
+                <div class="icon icon1"></div>
+                <div class="icon icon2"></div>
+                <div class="icon icon3"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

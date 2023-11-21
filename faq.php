d181187d1efd0570be6cf51e0430b2dc47d8c9cb
Manohara K G W Y <warushayohan80@gmail.com>
2023-11-21 11:08:40 +0530

    Yohan dupport (#50)
    * first
    
    * support page csss issues are fixed
    
    * dd
    
    * e
    
    * ww
    
    * bug
    
    * sws
    
    * last bug fixed back end
    
    * email change
    
    ---------
    
    Co-authored-by: yohan manohara <warushayoham80@gmail.com>

--- faq.php
+++ faq.php
@@ -1,6 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
+        
+    
+    <script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+      </script>
+          <script type="text/javascript">
+              (function(){
+              emailjs.init("h4DNcZvqdz8pXDohG");
+            })();
+      </script>
+      <script src="./js/script.js"></script>
+
+
+    
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <title>soundsmartai</title>
@@ -74,16 +88,11 @@
                     <br>
 
 
-                 <div class="bt"><input type="submit" value="submit" ></div>
+                 <div class="bt"><input type="submit" value="submit" onclick="sendEmail()" ></div>
 
 
                 
-
             
-          </form>
-            
-
-
 
 
         </div>

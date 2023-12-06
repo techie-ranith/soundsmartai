06e32192c14b3e714f617abe366a4d9f0fcdaadd
yohan manohara <warushayoham80@gmail.com>
2023-12-07 02:59:18 +0530

    about page make 70%

--- about.php
+++ about.php
@@ -1,7 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+
+    <html lang="en">
 <head>
-    <!-- Google tag (gtag.js) -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/about.css">
+  <!-- mediaqurees -->
+
+<!-- tailwinf -->
+<script src="https://cdn.tailwindcss.com"></script>
+
+<!-- animation -->
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-48LQRBV96R"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -10,22 +24,10 @@
 
   gtag('config', 'G-48LQRBV96R');
 </script>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="./css/about.css">
-  <!-- mediaqurees -->
-<link rel="stylesheet" href="./css/mediaquree.css">
-<!-- tailwinf -->
-<script src="https://cdn.tailwindcss.com"></script>
-
-<!-- animation -->
-
 
 
 </head>
 <body>
-   
 <?php
     include "cursor.php";
     ?>
@@ -34,45 +36,44 @@
     include "navbar.php";
     ?>
 
-
 <br><br><br>   
-   <div class="container1">
+   <div class="container1" style="float: right;">
       <div class="section1">
           <div class="aboutus"><div class="abt">About US</div>
-          <div>
+          
               <p>
               Welcome to our world of innovation where the spoken word meets cutting-edge artificial intelligence.
                We are passionate about transforming speech into text, and we're excited to share our journey with you.
               </p>
-          </div>
+          
       </div>
   </div>
    </div>
 
-  <div class="container2" style="float: right;">
+  <div class="container2" style="float: left;">
       <div class="section2">
           <div class="abt">Our Story</div>
-          <div>
+          
             <p>Our story begins with a vision - a vision to make communication more accessible and efficient. 
               With a dedicated team of experts in artificial intelligence, 
               machine learning, and natural language processing, 
               we set out to create an AI speech-to-text converter that would not only
                transcribe spoken words but also elevate the way people communicate.
-            </p></div>
+            </p>
       </div>
   </div>
 
-<div class="container3">
+<div class="container3" style="float: right;">
     <div class="section3">
         <div class="abt">Our Mission</div>
-        <div>
+        
             <p>At our core, we are driven by a simple yet powerful
              mission: to empower individuals and businesses to unlock 
              the potential of their spoken content. Whether you're a 
              journalist, a student, a medical professional, or an 
              entrepreneur, we are here to assist you in converting 
              spoken words into written text effortlessly and accurately.
-        </p></div>
+        </p>
     </div>
 </div>
 
@@ -80,14 +81,14 @@
     <img src="./assest/image 25.png" height="200" width="250" style="float: right;width:300px;height:300px;">
 </div>
 
-<div class="container4">
+<div class="container4" style="float: left;">
     <div class="section4">
         <div class="abt">Customization</div>
-        <div><p>
+        <p>
             We understand that one size doesn't fit all.
             That's why we offer customization options, allowing you to tailor the AI model to your specific needs, whether it's industry-specific jargon or accent recognition.
 
-        </p></div>
+        </p>
     </div>
 
 </div>
@@ -95,21 +96,21 @@
 <div class="container5" style="float:right;">
     <div class="section5">
         <div class="abt">Security</div>
-        <div><p>
+        <p>
             Your data privacy is paramount to us. 
             We employ robust security measures to protect your information and maintain confidentiality.
-        </p></div>
+        </p>
     </div>
 </div>
 
 <div class="container6">
     <div class="section6">
         <div class="abt">Accuracy</div>
-        <div><p>
+        <p>
             Our AI speech-to-text converter is engineered to deliver exceptional accuracy, 
             even in diverse linguistic environments and challenging audio conditions.
 
-        </p></div>
+        </p>
     </div>
 
 </div>
@@ -117,10 +118,10 @@
 <div class="container7" style="float: right;">
     <div class="section7">
         <div class="abt">Ease of use</div>
-        <div><p>
+        <p>
             Our user-friendly interface and developer-friendly APIs make integration seamless, 
             ensuring you get up and running quickly.
-        </p></div>
+        </p>
     </div>
 </div>
 
@@ -131,20 +132,15 @@
 <div class="container8">
     <div class="section8">
         <div class="abt">Join With Us</div>
-        <div><p>
+        <p>
             We're not just about technology; we're about the people who use it. 
             Our AI speech-to-text converter is designed to enhance your productivity, accessibility, and communication. 
             We invite you to join us on this transformative journey of turning spoken words into written text with unparalleled ease.
 
-        </p></div>
+        </p>
     </div>
 </div>
-
-
 <?php include "footer.php"; ?>
-
-
-
-
 </body>
+
 </html>

57c955daf89e96b5dc6f333099dbcbc8f7aa067d
yohan manohara <warushayoham80@gmail.com>
2023-12-06 17:27:26 +0530

    bug fixed with the icon

--- navbar.php
+++ navbar.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Soundsmartai</title>
-  <link rel="icon" href="./assest/01.png">
+  
+  <link rel="icon" href="./assest/01.png" type="image/png">
   
   <link rel="stylesheet" href="./css/navbar.css">
   <link rel="stylesheet" href="./css/animation.css">

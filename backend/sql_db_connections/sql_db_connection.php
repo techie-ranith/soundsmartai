1ea17810235d71024c17938b66b778ebea9d53a0
yohan manohara <warushayoham80@gmail.com>
2023-12-10 12:37:50 +0530

    manage database

--- backend/sql_db_connections/sql_db_connection.php
+++ backend/sql_db_connections/sql_db_connection.php
@@ -2,7 +2,7 @@
 $host = "localhost"; 
 $username = "root"; 
 $password = ""; 
-$database = "soundsmartai"; 
+$database = "soun_soundsmartai"; 
 
 
 $conn = new mysqli($host, $username, $password, $database);
